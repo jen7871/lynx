34c95bf1ae8d8f03bf1af88e738e1a8c5718823e
excel <admin@excel>
2024-06-28 11:12:25 +0800

    fix graphmodel

--- src/main/scala/org/grapheco/lynx/runner/GraphModel.scala
+++ src/main/scala/org/grapheco/lynx/runner/GraphModel.scala
@@ -108,8 +108,8 @@ trait GraphModel {
   def setNodesLabels(nodeIds: Iterator[LynxId], labels: Array[String]): Iterator[Option[LynxNode]] =
     this.write.setNodesLabels(nodeIds, labels.map(LynxNodeLabel))
 
-  def setRelationshipsProperties(relationshipIds: Iterator[LynxId], data: Array[(String, LynxValue)]): Iterator[Option[LynxRelationship]] =
-    this.write.setRelationshipsProperties(relationshipIds, data.map(kv => (LynxPropertyKey(kv._1), kv._2)))
+  def setRelationshipsProperties(relationshipIds: Iterator[LynxId], data: Array[(String, LynxValue)], cleanExistProperties: Boolean = false): Iterator[Option[LynxRelationship]] =
+    this.write.setRelationshipsProperties(relationshipIds, data.map(kv => (LynxPropertyKey(kv._1), kv._2)), cleanExistProperties)
 
   def setRelationshipsType(relationshipIds: Iterator[LynxId], theType: String): Iterator[Option[LynxRelationship]] =
     this.write.setRelationshipsType(relationshipIds, LynxRelationshipType(theType))
